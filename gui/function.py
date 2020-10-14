c031a1c53ab5c10769ea32e674865f3fe54efe23
Tornier Alexandre <alexandretornier6@gmail.com>
2020-10-14 17:22:28 +0200

    Update Graphique
    Session 14/10 création des cases (coordonnées) et pions.

--- gui/function.py
+++ gui/function.py
@@ -18,26 +18,58 @@ def CreationDamier(gui, SW, SH):
     b = range(int(SH*0.1), int(SH*0.6), int(SW*0.03))
     compteur1 = 1
     compteur2 = 1
-
+    coordonnees=[]
     for i in a:
         if compteur1%2 == 0:
             for j in b:
                 if compteur2 % 2 == 0:
+                    z = int(SW * 0.03)
                     gui.create_rectangle(i, j, i+int(SW*0.03), j+int(SW*0.03), fill="white", outline="black")
+                    coordonnees.append([i, j, i+z, j+z, 0])
                 else:
+                    z = int(SW * 0.03)
                     gui.create_rectangle(i, j, i+int(SW*0.03), j+int(SW*0.03), fill="black", outline="black")
+                    coordonnees.append([i, j, i+z, j+z, 0])
                 compteur2 += 1
             compteur1 += 1
         else:
             for j in b:
                 if compteur2 % 2 == 0:
+                    z = int(SW * 0.03)
                     gui.create_rectangle(i, j, i+int(SW*0.03), j+int(SW*0.03), fill="black", outline="black")
+                    coordonnees.append([i, j, i+z, j+z, 0])
                 else:
+                    z = int(SW * 0.03)
                     gui.create_rectangle(i, j, i+int(SW*0.03), j+int(SW*0.03), fill="white", outline="black")
+                    coordonnees.append([i, j, i+z, j+z, 0])
                 compteur2 += 1
             compteur1 += 1
 
-    return
+    return coordonnees
+
+
+def creerpion(gui, SW, SH):
+    Coord = CreationDamier(gui, SW, SH)
+    for i in range(0, 10):
+        if i%2 != 0:
+            for j in range(0, 3, 2):
+                j = j + i * 10
+                gui.create_oval(Coord[j][0], Coord[j][1], Coord[j][2], Coord[j][3], fill="Red")
+                Coord[j][4] = 1
+            for j in range(8, 10, 2):
+                j = j + i * 10
+                gui.create_oval(Coord[j][0], Coord[j][1], Coord[j][2], Coord[j][3], fill="Blue")
+                Coord[j][4] = 2
+        if i%2 == 0:
+            for j in range(1, 3, 2):
+                j = j + i * 10
+                gui.create_oval(Coord[j][0], Coord[j][1], Coord[j][2], Coord[j][3], fill="Red")
+                Coord[j][4] = 1
+            for j in range(7, 10, 2):
+                j = j + i * 10
+                gui.create_oval(Coord[j][0], Coord[j][1], Coord[j][2], Coord[j][3], fill="Blue")
+                Coord[j][4] = 2
+
 
 def CompteurTemps(temps, gui, SW, SH):
     TexteStyle = tkFont.Font(family="Lucida Grande", size=20)
@@ -65,12 +97,20 @@ async def timer():
         if secondes == 59:
             minutes += 1
 
+def position(event):
+    print("position du pointeur:", event.x, event.y)
+
+
 def Start():
     Interface, Window, SW, SH = Canva()
     CompteurTemps("0:00", Interface, SW, SH)
     Joueur1("Hugo", Interface, SW, SH)
     Joueur2("Alexandre", Interface, SW, SH)
     CreationDamier(Interface, SW, SH)
+    creerpion(Interface, SW, SH)
+
+    Window.bind("<Button-1>", position)
+    Window.bind("<ButtonRelease>", position)
     Window.mainloop()
 
 Start()
